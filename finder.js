f9e4101e327c76eb5d1238c930d50845fbdc1e5e
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-08-17 11:50:39 +0100

    add .getProjects() method to pull in projects from multiple endpoints

--- finder.js
+++ finder.js
@@ -4,7 +4,7 @@ import search from './lib/search';
 import config from './config';
 const searchString = process.argv[2];
 
-serviceCatalogue.getServices()
+serviceCatalogue.getProjects(config.api.paths)
   .then(services => services.filter(service => isFrontendService(service.name, config.whitelist)))
   .then(frontendServices => serviceCatalogue.clone(frontendServices))
   .then(() => search(searchString))
--- test/serviceCatalogue.test.js
+++ test/serviceCatalogue.test.js
@@ -3,9 +3,9 @@ import nock from 'nock';
 import sinon from 'sinon';
 import config from './../config';
 import serviceCatalogue from './../lib/serviceCatalogue';
-import isFrontendService from './../lib/utils/isFrontendService';
-
+const [servicesAPIPath, librariesAPIPath] = config.api.paths;
 let mockServices = [];
+let mockLibraries = [];
 
 test.beforeEach(t => {
   mockServices = [
@@ -30,16 +30,27 @@ test.beforeEach(t => {
       ],
     }
   ];
+
+  mockLibraries = [
+    {
+      name: 'testLibrary',
+      githubUrls: [
+        {
+          name: 'github-com',
+          displayName: 'GitHub.com',
+          url: 'https://github.com/test-url'
+        }
+      ],
+    }
+  ];
 });
 
 test('.getServices() should successfully return services', async t => {
-  t.plan(2);
-
   nock(`${config.api.protocol}://${config.api.host}:443`)
-    .get(config.api.path)
+    .get(servicesAPIPath)
     .reply(200, mockServices);
 
-  const services = await serviceCatalogue.getServices();
+  const services = await serviceCatalogue.getServices(servicesAPIPath);
 
   t.true(services instanceof Array);
   t.deepEqual(services, mockServices);
@@ -47,10 +58,10 @@ test('.getServices() should successfully return services', async t => {
 
 test('.getServices() should error/reject when statusCode is not 200', t => {
   nock(`${config.api.protocol}://${config.api.host}:443`)
-    .get(config.api.path)
+    .get(servicesAPIPath)
     .reply(404, 'URI not found');
 
-  return serviceCatalogue.getServices()
+  return serviceCatalogue.getServices(servicesAPIPath)
     .catch(err => {
       // there is no way of adding statusMessage directly to the response https://github.com/node-nock/nock/issues/469
       t.is(err, 'ResponseCode: 404, StatusMessage: null');
@@ -59,10 +70,10 @@ test('.getServices() should error/reject when statusCode is not 200', t => {
 
 test('.getServices() should reject when bad JSON is returned', t => {
   nock(`${config.api.protocol}://${config.api.host}:443`)
-    .get(config.api.path)
+    .get(servicesAPIPath)
     .reply(200, 'badJSON');
 
-  return serviceCatalogue.getServices()
+  return serviceCatalogue.getServices(servicesAPIPath)
     .catch(err => {
       t.is(err.name, 'SyntaxError');
       t.is(err.message, 'Unexpected token b');
@@ -71,10 +82,10 @@ test('.getServices() should reject when bad JSON is returned', t => {
 
 test('.getServices() should reject when errors are returned', t => {
   nock(`${config.api.protocol}://${config.api.host}:443`)
-    .get(config.api.path)
+    .get(servicesAPIPath)
     .replyWithError('something awful happened');
 
-  return serviceCatalogue.getServices()
+  return serviceCatalogue.getServices(servicesAPIPath)
     .catch(err => {
       t.is(err.message, 'something awful happened');
     });
@@ -87,7 +98,7 @@ test('.clone() should resolve 2 cloneTask promises', async t => {
     });
   });
 
-  const pr = await serviceCatalogue.clone(mockServices, cloneTaskSpy);
+  await serviceCatalogue.clone(mockServices, cloneTaskSpy);
 
   t.true(cloneTaskSpy.calledTwice);
 });
@@ -106,4 +117,19 @@ test('.clone() should error when a cloneTask promise is rejected', async t => {
       t.true(err instanceof Error);
       t.is(err.message, mockError);
     });
+});
+
+test('.getProjects() should return an array of projects', async t => {
+  const serviceScope = nock(`${config.api.protocol}://${config.api.host}:443`)
+    .get(servicesAPIPath)
+    .reply(200, mockServices);
+
+  const libScope = nock(`${config.api.protocol}://${config.api.host}:443`)
+    .get(librariesAPIPath)
+    .reply(200, mockLibraries);
+
+  const projects = await serviceCatalogue.getProjects(config.api.paths);
+
+  t.true(Array.isArray(projects));
+  t.is(projects.length, mockServices.length + mockLibraries.length);
 });
