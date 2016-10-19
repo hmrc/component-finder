05d829f187e3c6ca6062a5214dfdb526762aba7f
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-10-19 17:26:06 +0100

    search for files via glob

---
+++ test/FindFiles.test.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+import sinon from 'sinon';
+import glob from 'glob';
+import minimatch from 'minimatch';
+import FindFiles from './../lib/streams/FindFiles';
+
+const files = [
+  'test1.html',
+  'test2.js',
+  'node_modules/test3.html',
+  'target/test4.html',
+  'target/test5.js'
+];
+const globSyncStub = sinon.stub(glob, 'sync', (pattern) => {
+  return files.filter((file) => minimatch(file, pattern));
+});
+
+test.after('cleanup', t => globSyncStub.restore());
+
+test('file should be html file inside target dir', async t => {
+  const findFiles = new FindFiles({objectMode: true}, 'target/**/*.html');
+
+  await findFiles
+    .on('data', file => t.is(file, files[3]));
+});
+
+test('files should be returned line by line', async t => {
+  const findFiles = new FindFiles({objectMode: true}, '*');
+  let count = 0;
+
+  await findFiles
+    .on('data', file => t.is(file, files[count++]));
+});
