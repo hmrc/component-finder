a11874ac9c620260ecc84e8ce2da05d2a1b4e888
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-10-19 16:00:36 +0100

    Steam to provide array data item by item to consumer stream

---
+++ test/ItemByItem.test.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import ItemByItem from './../lib/streams/ItemByItem';
+import {PassThrough} from 'stream';
+
+const passThrough = new PassThrough({objectMode: true});
+const itemByItem = new ItemByItem({objectMode: true});
+const items = [
+  'line1',
+  'line2',
+  'line3'
+];
+
+test('output from stream should be provided to consumer item by item', async t => {
+  let count = 0;
+
+  passThrough.write(items);
+  passThrough.end();
+
+  await passThrough
+    .pipe(itemByItem)
+    .on('data', item => t.is(item, items[count++]));
+});
