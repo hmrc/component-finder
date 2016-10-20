d5972e271830f401c32a100c0c0ef3082c92e549
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2016-10-20 02:28:25 +0100

    Improve error handling around the existence (or not) of a config file

--- finder.js
+++ finder.js
@@ -1,8 +1,17 @@
-import config from './config';
+import fs from 'fs';
+import {EOL} from 'os';
 import clone from './lib/clone';
 import search from './lib/search';
 import serviceCatalogue from './lib/serviceCatalogue';
 import isFrontendService from './lib/utils/isFrontendService';
+
+try {
+  var config = require('./config.json');
+}
+catch(err) {
+  process.stdout.write(`No config.json file found ${EOL}`);
+  process.exit();
+};
 
 const searchString = process.argv[2];
 
